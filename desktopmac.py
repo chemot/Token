3de369aa4cb9fa827fe2b6a6b6ff3cac573cd350
chemot <40890269+chemot@users.noreply.github.com>
2018-07-23 21:24:57 +0700

    Update desktopmac.py

--- desktopmac.py
+++ desktopmac.py
@@ -4,7 +4,7 @@ from thrift.transport import THttpClient
 from ttypes import LoginRequest
 import json, requests, LineService
 
-nama = 'PrankBots-Pc-DesktopMac'
+nama = 'Sepri_PC_DesktopMac'
 Headers = {
         'User-Agent': "Line/8.3.2",
         'X-Line-Application': "DESKTOPMAC\t8.3.2\t"+nama+"\t10.0.0",
